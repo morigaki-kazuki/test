1f06c9f6179c52d4187170398362303dd8183086
morigaki-kazuki <morigaki-kazuki@scnet.co.jp>
2021-10-19 16:27:28 +0900

    test03

--- code.js
+++ code.js
@@ -1,3 +1,8 @@
-document.getElementById("baz").addEventListener("click",function(){
-    alert('aaaaaa');
-})
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
